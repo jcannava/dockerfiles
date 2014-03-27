0fea86369fdc1f88074fb0d680ca5d8d5a666f96
jcannava <jason@cannavale.com>
2014-03-27 16:29:57 -0500

    more fixerating keystone

--- docker_setup.sh
+++ docker_setup.sh
@@ -5,3 +5,12 @@ sudo apt-get install linux-image-generic-lts-raring linux-headers-generic-lts-ra
 
 # Install docker
 curl -s https://get.docker.io/ubuntu/ | sudo sh
+
+# Clone DockerFiles
+git clone https://github.com/jcannava/dockerfiles
+
+# Build Services
+# Keystone
+cd dockerfiles/keystone
+docker build -t keystone .
+docker run -d -P keystone
