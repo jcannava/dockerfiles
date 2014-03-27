d3bda9f75a160426d436471361c9b6598e744228
jcannava <jason@cannavale.com>
2014-03-27 16:37:34 -0500

    adding glance

--- docker_setup.sh
+++ docker_setup.sh
@@ -6,11 +6,15 @@ sudo apt-get install linux-image-generic-lts-raring linux-headers-generic-lts-ra
 # Install docker
 curl -s https://get.docker.io/ubuntu/ | sudo sh
 
-# Clone DockerFiles
-git clone https://github.com/jcannava/dockerfiles
-
 # Build Services
+
 # Keystone
 cd dockerfiles/keystone
 docker build -t keystone .
 docker run -d -P keystone
+
+# Glance
+cd ../glance
+docker build -t glance .
+docker run -d -P glance
+
